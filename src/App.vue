e2e7308d0352c75f8e9624fa8334c90dcfe51975
Давид Элиаури <eliauri.david@gmail.com>
2023-05-11 11:50:37 +0300

    added initial template

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,196 @@
 <script setup></script>
 
 <template>
-    <h1 class="text-sm">Cryptonomicon</h1>
+    <div class="container mx-auto flex flex-col items-center bg-gray-100 p-4">
+        <div class="container">
+            <section>
+                <div class="flex">
+                    <div class="max-w-xs">
+                        <label
+                            for="wallet"
+                            class="block text-sm font-medium text-gray-700"
+                            >Тикер</label
+                        >
+                        <div class="relative mt-1 rounded-md shadow-md">
+                            <input
+                                type="text"
+                                name="wallet"
+                                id="wallet"
+                                class="block w-full rounded-md border-gray-300 pr-10 text-gray-900 focus:border-gray-500 focus:outline-none focus:ring-gray-500 sm:text-sm"
+                                placeholder="Например DOGE"
+                            />
+                        </div>
+                    </div>
+                </div>
+                <button
+                    type="button"
+                    class="my-4 inline-flex items-center rounded-full border border-transparent bg-gray-600 px-4 py-2 text-sm font-medium leading-4 text-white shadow-sm transition-colors duration-300 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2"
+                >
+                    <!-- Heroicon name: solid/mail -->
+                    <svg
+                        class="-ml-0.5 mr-2 h-6 w-6"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="30"
+                        height="30"
+                        viewBox="0 0 24 24"
+                        fill="#ffffff"
+                    >
+                        <path
+                            d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"
+                        ></path>
+                    </svg>
+                    Добавить
+                </button>
+            </section>
+
+            <hr class="my-4 w-full border-t border-gray-600" />
+
+            <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+                <div
+                    class="cursor-pointer overflow-hidden rounded-lg border-solid border-purple-800 bg-white shadow"
+                >
+                    <div class="px-4 py-5 text-center sm:p-6">
+                        <dt class="truncate text-sm font-medium text-gray-500">WTF - USD</dt>
+                        <dd class="mt-1 text-3xl font-semibold text-gray-900">1.11</dd>
+                    </div>
+                    <div class="w-full border-t border-gray-200"></div>
+                    <button
+                        class="text-md flex w-full items-center justify-center bg-gray-100 px-4 py-4 font-medium text-gray-500 transition-all hover:bg-gray-200 hover:text-gray-600 hover:opacity-20 focus:outline-none sm:px-6"
+                    >
+                        <svg
+                            class="h-5 w-5"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="#718096"
+                            aria-hidden="true"
+                        >
+                            <path
+                                fill-rule="evenodd"
+                                d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                                clip-rule="evenodd"
+                            ></path>
+                        </svg>
+                        Удалить
+                    </button>
+                </div>
+                <div
+                    class="cursor-pointer overflow-hidden rounded-lg border-4 border-solid border-purple-800 bg-white shadow"
+                >
+                    <div class="px-4 py-5 text-center sm:p-6">
+                        <dt class="truncate text-sm font-medium text-gray-500">VUE - RUB</dt>
+                        <dd class="mt-1 text-3xl font-semibold text-gray-900">80000.00</dd>
+                    </div>
+                    <div class="w-full border-t border-gray-200"></div>
+                    <button
+                        class="text-md flex w-full items-center justify-center bg-gray-100 px-4 py-4 font-medium text-gray-500 transition-all hover:bg-gray-200 hover:text-gray-600 hover:opacity-20 focus:outline-none sm:px-6"
+                    >
+                        <svg
+                            class="h-5 w-5"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="#718096"
+                            aria-hidden="true"
+                        >
+                            <path
+                                fill-rule="evenodd"
+                                d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                                clip-rule="evenodd"
+                            ></path>
+                        </svg>
+                        Удалить
+                    </button>
+                </div>
+                <div
+                    class="cursor-pointer overflow-hidden rounded-lg border-solid border-purple-800 bg-white shadow"
+                >
+                    <div class="px-4 py-5 text-center sm:p-6">
+                        <dt class="truncate text-sm font-medium text-gray-500">BTC - USD</dt>
+                        <dd class="mt-1 text-3xl font-semibold text-gray-900">99999.99</dd>
+                    </div>
+                    <div class="w-full border-t border-gray-200"></div>
+                    <button
+                        class="text-md flex w-full items-center justify-center bg-gray-100 px-4 py-4 font-medium text-gray-500 transition-all hover:bg-gray-200 hover:text-gray-600 hover:opacity-20 focus:outline-none sm:px-6"
+                    >
+                        <svg
+                            class="h-5 w-5"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="#718096"
+                            aria-hidden="true"
+                        >
+                            <path
+                                fill-rule="evenodd"
+                                d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                                clip-rule="evenodd"
+                            ></path>
+                        </svg>
+                        Удалить
+                    </button>
+                </div>
+                <div
+                    class="cursor-pointer overflow-hidden rounded-lg border-solid border-purple-800 bg-white shadow"
+                >
+                    <div class="px-4 py-5 text-center sm:p-6">
+                        <dt class="truncate text-sm font-medium text-gray-500">DOGE - USD</dt>
+                        <dd class="mt-1 text-3xl font-semibold text-gray-900">0.0014</dd>
+                    </div>
+                    <div class="w-full border-t border-gray-200"></div>
+                    <button
+                        class="text-md flex w-full items-center justify-center bg-gray-100 px-4 py-4 font-medium text-gray-500 transition-all hover:bg-gray-200 hover:text-gray-600 hover:opacity-20 focus:outline-none sm:px-6"
+                    >
+                        <svg
+                            class="h-5 w-5"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="#718096"
+                            aria-hidden="true"
+                        >
+                            <path
+                                fill-rule="evenodd"
+                                d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                                clip-rule="evenodd"
+                            ></path>
+                        </svg>
+                        Удалить
+                    </button>
+                </div>
+            </dl>
+            <hr class="my-4 w-full border-t border-gray-600" />
+            <section class="relative">
+                <h3 class="my-8 text-lg font-medium leading-6 text-gray-900">VUE - USD</h3>
+                <div class="flex h-64 items-end border-b border-l border-gray-600">
+                    <div class="h-24 w-10 border bg-purple-800"></div>
+                    <div class="h-32 w-10 border bg-purple-800"></div>
+                    <div class="h-48 w-10 border bg-purple-800"></div>
+                    <div class="h-16 w-10 border bg-purple-800"></div>
+                </div>
+                <button
+                    type="button"
+                    class="absolute right-0 top-0"
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        xmlns:xlink="http://www.w3.org/1999/xlink"
+                        xmlns:svgjs="http://svgjs.com/svgjs"
+                        version="1.1"
+                        width="30"
+                        height="30"
+                        x="0"
+                        y="0"
+                        viewBox="0 0 511.76 511.76"
+                        style="enable-background: new 0 0 512 512"
+                        xml:space="preserve"
+                    >
+                        <g>
+                            <path
+                                d="M436.896,74.869c-99.84-99.819-262.208-99.819-362.048,0c-99.797,99.819-99.797,262.229,0,362.048    c49.92,49.899,115.477,74.837,181.035,74.837s131.093-24.939,181.013-74.837C536.715,337.099,536.715,174.688,436.896,74.869z     M361.461,331.317c8.341,8.341,8.341,21.824,0,30.165c-4.16,4.16-9.621,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    l-75.413-75.435l-75.392,75.413c-4.181,4.16-9.643,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    c-8.341-8.341-8.341-21.845,0-30.165l75.392-75.413l-75.413-75.413c-8.341-8.341-8.341-21.845,0-30.165    c8.32-8.341,21.824-8.341,30.165,0l75.413,75.413l75.413-75.413c8.341-8.341,21.824-8.341,30.165,0    c8.341,8.32,8.341,21.824,0,30.165l-75.413,75.413L361.461,331.317z"
+                                fill="#718096"
+                                data-original="#000000"
+                            ></path>
+                        </g>
+                    </svg>
+                </button>
+            </section>
+        </div>
+    </div>
 </template>
